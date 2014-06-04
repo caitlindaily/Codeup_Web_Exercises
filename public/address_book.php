e76600ec629e3660382529e6dd986f289322ae92
sweetrocker22 <cdaily87@gmail.com>
2014-06-03 23:53:25 -0500

    Created form for an upload

--- public/address_book.php
+++ public/address_book.php
@@ -1,10 +1,19 @@
 <?php
+
+$addressBook = [];
+$errorMessage = '';
+
 class AddressDataStore 
 {
 
 	public $addressBook = [];
 	public $filename = 'address_book.csv';
 	public $newAddress = [];
+
+	public function __construct($filename = 'address_book.csv')
+	{
+		$this->filename = $filename;
+	}
 
 	public function read() 
 	{
@@ -30,16 +39,40 @@ class AddressDataStore
 	}
 }
 
-$addressDataStore = new AddressDataStore();
-$addressBook = $addressDataStore->read();
+$ads = new AddressDataStore();
+$addressBook = $ads->read();
 
+if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
+	if ($_FILES['file1']['type'] == 'text/plain') {
+		$upload_directory = '/vagrant/sites/codeup.dev/public/uploads/';
+		$filename = basename($_FILES['file1']['name']);
+		$saved_file = $upload_directory . $filename;
+		move_uploaded_file($_FILES['file1']['tmp_name'], $saved_file);	
+		
+		$uploaded_file = open_list($saved_file);
+		$todo_items = array_merge($todo_items, $uploaded_file);
+		save_list($todo_items);
+	}else {
+		echo "Please upload plain text file only.";
+	}
+}
 
 if (!empty($_POST)) {
 	foreach($_POST as $value) {
 		$newAddress[] = $value;
 	}
 	array_push($addressBook, $newAddress);
-	$addressDataStore->write($addressBook);
+	$ads->write($addressBook);
+}
+
+if(isset($_GET['index']))
+{
+	$index = $_GET['index'];
+	unset($addressBook[$index]);
+	$addressBook = array_values($addressBook);
+	$ads->write($addressBook);
+	header('Location: /address_book.php');
+	exit(0);
 }
 
 var_dump($_POST);
@@ -52,19 +85,26 @@ var_dump($_POST);
 </head>
 <body>
 	<table>
-		<? foreach ($addressBook as $entry) : ?>
 		<tr>
-			<? foreach ($entry as $value) :?>
-				<td><?=$value;?></td>
-			<? endforeach; ?>	
+		   <th>Name</th>
+		   <th>Address</th>
+		   <th>City</th>
+		   <th>State</th>
+		   <th>Zip</th>
+		   <th>Phone</th>
 		</tr>
+		<? foreach($addressBook as $index => $contact): ?>
+			<tr>
+				<? foreach ($contact as $value): ?>
+					 <td> <?= htmlspecialchars(strip_tags($value)) ?> </td>
+				<? endforeach; ?>
+				<td><?="<a href=\"/address_book.php?index={$index}\">Remove Item</a>";?></td>
+			</tr>
 		<? endforeach; ?>
+		</tr>
 	</table>
 <form method="POST">
 	<h1>Address Book</h1>
-	<p>
-			
-	</p>
 	<p>
 		<p>
 			<label for="name">Full Name </label>
@@ -93,8 +133,17 @@ var_dump($_POST);
 		<p>
 			<button type="submit">Add New Entry</button>
 		<p/>
-	</p>	
 </form>
+<form method="POST" enctype="multipart/form-data">
+	<h2>Upload a File</h2>
+		<p>	
+			<label for="file1">Upload File: </label>
+			<input type="file" id="file1" name="file1">
+		</p>
+		<p>
+			<input type="submit" value="upload">
+		</p>		
+	</form>	
 </body>
 </html>
 
